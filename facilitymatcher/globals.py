036c808924e808969f5153078c889c676bd311b5
WesIngwersen <ingwersen.wesley@epa.gov>
2018-05-25 16:38:44 -0400

    add download_extract

--- facilitymatcher/globals.py
+++ facilitymatcher/globals.py
@@ -1,3 +1,13 @@
+import zipfile
+import io
+import requests
+
+def download_extract_FRS_single_national(FRSpath):
+    url = 'https://www3.epa.gov/enviro/html/fii/downloads/state_files/national_combined.zip'
+    request = requests.get(url).content
+    zip_file = zipfile.ZipFile(io.BytesIO(request))
+    zip_file.extractall(FRSpath)
+
 def filter_bridges_by_program_list(bridges,program_list):
     bridges = bridges[bridges['PGM_SYS_ACRNM'].isin(program_list)]
     return bridges
