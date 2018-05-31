4c97ebf0f3cc2b1620a12ec3beb4ef3e41e8dd12
WesIngwersen <ingwersen.wesley@epa.gov>
2018-05-31 11:18:44 -0400

    Add functions to work for inventory acronyms and not just programs

--- facilitymatcher/globals.py
+++ facilitymatcher/globals.py
@@ -2,6 +2,8 @@ import zipfile
 import io
 import requests
 
+inventory_to_FRS_pgm_acronymn = {"NEI":"EIS","TRI":"TRIS","eGRID":"EGRID","GHGRP":"E-GGRT","RCRAInfo":"RCRAINFO","DMR":"NPDES"}
+
 def download_extract_FRS_single_national(FRSpath):
     url = 'https://www3.epa.gov/enviro/html/fii/downloads/state_files/national_combined.zip'
     request = requests.get(url).content
@@ -12,6 +14,10 @@ def filter_bridges_by_program_list(bridges,program_list):
     bridges = bridges[bridges['PGM_SYS_ACRNM'].isin(program_list)]
     return bridges
 
+def filter_bridges_by_inventory_list(bridges,inventory_list):
+    bridges = bridges[bridges['PGM_SYS_ACRNM'].isin(inventory_list)]
+    return bridges
+
 def filter_bridges_by_facility_list(bridges,facility_list):
     bridges = bridges[bridges['REGISTRY_ID'].isin(facility_list)]
     return bridges
@@ -19,3 +25,17 @@ def filter_bridges_by_facility_list(bridges,facility_list):
 def list_facilities_not_in_bridge(bridges, facility_list):
     facilities = bridges[bridges['REGISTRY_ID'].isin(facility_list)]
     return bridges
+
+#Returns list of acronymns for inventories that correspond to
+def get_programs_for_inventory_list(list_of_inventories):
+    program_list = []
+    for l in list_of_inventories:
+        pgm_acronym = inventory_to_FRS_pgm_acronymn[l]
+        program_list.append(pgm_acronym)
+    return program_list
+
+def invert_inventory_to_FRS():
+    FRS_to_inventory_pgm_acronymn = {v: k for k, v in inventory_to_FRS_pgm_acronymn.items()}
+    return FRS_to_inventory_pgm_acronymn
+
+
