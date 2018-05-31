3d2bcc59b349ed1baf1b691671c6465f9e2f7e18
WesIngwersen <ingwersen.wesley@epa.gov>
2018-05-31 11:17:45 -0400

    Clean up add os reference

--- facilitymatcher/WriteFRSBridge.py
+++ facilitymatcher/WriteFRSBridge.py
@@ -1,5 +1,10 @@
-#Write facility matching file for StEWI programs
+#This script gets FRS data in the form of the FRS combined national files
+#https://www.epa.gov/enviro/epa-state-combined-csv-download-files
+#It uses the bridges in the 'NATIONAL_ENVIRONMENTAL_INTEREST_FILE.CSV'
+#It writes facility matching file for StEWI (github.com/usepa/standardizedinventories) programs
+
 import pandas as pd
+import os
 
 from facilitymatcher.globals import filter_bridges_by_program_list
 from facilitymatcher.globals import download_extract_FRS_single_national
@@ -11,17 +16,13 @@ FRS_bridge_file = 'NATIONAL_ENVIRONMENTAL_INTEREST_FILE.CSV'
 FRS_bridge_file_path = FRSpath + FRS_bridge_file
 
 #Check to see if file exists
-if !os.path.exists(FRS_bridge_file_path):
+if not(os.path.exists(FRS_bridge_file_path)):
     download_extract_FRS_single_national()
-
-#This script gets FRS data in the form of the FRS combined national files
-#https://www.epa.gov/enviro/epa-state-combined-csv-download-files
-#'NATIONAL_ENVIRONMENTAL_INTEREST_FILE.CSV' for bridges across programs and the
 
 #Import FRS bridge which provides ID matches
 FRS_Bridges = pd.read_csv(FRS_bridge_file_path, header=0,usecols=['REGISTRY_ID','PGM_SYS_ACRNM', 'PGM_SYS_ID',],dtype={'REGISTRY_ID':"str",'PGM_SYS_ACRNM':"str",'PGM_SYS_ID':"str"})
 #Or Load all bridges from pickle
-#FRS_Bridges = pd.read_pickle('frsbridges.pk')
+FRS_Bridges = pd.read_pickle('frsbridges.pk')
 
 #See programs available
 pd.unique(FRS_Bridges['PGM_SYS_ACRNM'])
