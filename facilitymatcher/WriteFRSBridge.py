660d8c2dbc4c0e322aec97cf0959bd881124beee
WesIngwersen <ingwersen.wesley@epa.gov>
2018-06-11 14:20:36 -0400

    Supplement egrid matches with EIA-860 FRS matches
    -Gets all EIA-860 matches for FRS facilities not in the the FRS EGRID
     matches

--- facilitymatcher/WriteFRSBridge.py
+++ facilitymatcher/WriteFRSBridge.py
@@ -31,12 +31,26 @@ programs_of_interest  = ['EIS','NPDES','TRIS','RCRAINFO','EGRID','E-GGRT','EIA-8
 
 FRS_Bridges = filter_bridges_by_program_list(FRS_Bridges,programs_of_interest)
 
+#Separate out eGRID and EIA-860 matches to identify EIA matches to add to eGRID set
+eia = filter_bridges_by_program_list(FRS_Bridges,['EIA-860'])
+egrid = filter_bridges_by_program_list(FRS_Bridges,['EGRID'])
 
-#Create hybrid eGRID and EIA-860 matches
-#eia = filter_bridges_by_program_list(FRS_Bridges,['EIA-860'])
-#egrid = filter_bridges_by_program_list(FRS_Bridges,['EGRID'])
+#get a list of all FRS in each
+eia_unique_frs = set(list(pd.unique(eia['REGISTRY_ID'])))
+egrid_unique_frs = set(list(pd.unique(egrid['REGISTRY_ID'])))
 
-#eia_egrid = pd.merge(eia,egrid,on='REGISTRY_ID',how='left')
+eia_not_in_egrid = eia_unique_frs - egrid_unique_frs
+eia_to_add = eia[eia['REGISTRY_ID'].isin(eia_not_in_egrid)]
+len(eia_to_add)
+eia_to_add.head(5)
+#Will add 1781
+
+#Rename to EGRID
+eia_to_add['PGM_SYS_ACRNM'] = 'EGRID'
+#Now add this subset back
+FRS_Bridges = pd.concat([FRS_Bridges,eia_to_add])
+
+len(FRS_Bridges[FRS_Bridges['PGM_SYS_ACRNM'] == 'EGRID'])
 
 
 
