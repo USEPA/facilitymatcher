d8d841297d6f41f70e5b744e94134876c601cf80
MLiadov <mike.liadov@erg.com>
2018-06-29 11:16:18 -0400

    update for encoding and in memory processing

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import csv
 import logging
 from datetime import datetime
 import enlighten
+import re
+import string
 
 
 seperator = " | "
@@ -37,7 +39,7 @@ if log:
 
 def read_in_chunks(filepath):
     chunksize = fields["CHUNK_SIZE"]
-    return pd.read_csv(filepath, chunksize=chunksize, iterator=True)
+    return pd.read_csv(filepath, chunksize=chunksize, iterator=True, encoding="utf-8")
 
 
 def combine_columns_as_str(df, fields, sep=" | "):
@@ -52,10 +54,13 @@ def combine_columns_as_str(df, fields, sep=" | "):
     return out.tolist()
 
 
-def write_row(row, filepath="output.csv"):
+def write_row(row, index, extra=None, filepath="output.csv"):
     global seperator
     row = row.split(seperator)
-
+    row.insert(0, index)
+    if extra:
+        row.extend(extra.split(seperator))
+    print(row, extra)
     if not os.path.exists(filepath):
         with open(filepath, 'a') as fp:
             pass    # create empty file
@@ -66,43 +71,47 @@ def write_row(row, filepath="output.csv"):
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             writer.writeheader()
-            writer.writerow({fieldnames[i]: row[i] for i in range(len(row))})
+            writer.writerow({fieldnames[i]: row[i] for i in range(len(fieldnames))})
 
     else:
         with open(filepath, 'a', newline='') as csvfile:
             fieldnames = fields["OUTPUT_FIELDS"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
             writer.writerow({fieldnames[i]: row[i] for i in range(len(row))})
 
 
-def filtersortfuzz(iterable, comparator, cutoff=80):
-    # f = lambda x: fuzz.ratio(x, comparator)
-    # f = lambda x: fuzz.partial_token_sort_ratio(x, comparator, force_ascii=True)
-    f = lambda x: fuzz.partial_ratio(x.lower(), comparator.lower())
-    # f = lambda x: 100*jellyfish.jaro_distance(x.lower(), comparator.lower())
+def filtersortfuzz(iterable, comparator, can_clean_text=True, cutoff=80):
+    def cleanup_text(s):
+        regex = re.compile('[%s]' % re.escape(string.punctuation))
+        s = regex.sub(' ', s)
+        return re.sub(' +', ' ', s).strip()
+
+    if can_clean_text:
+        f = lambda x: fuzz.partial_ratio(cleanup_text(x).lower(), cleanup_text(comparator).lower())
+    else:
+        f = lambda x: fuzz.partial_ratio(x.lower(), comparator.lower())
     out = sorted(iterable, key=f, reverse=True)
     maxscore = f(out[0])
-    return (out[0], maxscore) if maxscore >= cutoff else None
+    return (comparator, out[0], maxscore) if maxscore >= cutoff else None    # FIX earlier was returning out[0] only
 
 
 def main(verbose=False, no_log=False, skip_rows=0, skip_chunks=0, outputfile=None):
     FRS_filepath = fields["FRS_FILEPATH"]
     EIA_filepath = fields["EIA_FILEPATH"]
-    # print("EIA Type", type(EIA_filepath))
+
     log = not no_log
     if verbose:
         print("Reading file: ", FRS_filepath)
         print("Reading file: ", EIA_filepath)
 
     if log:
-        logger.info("Reading file: " + FRS_filepath)
-        logger.info("Reading file: " + EIA_filepath)
+        logger.info("Reading file: %r", FRS_filepath)
+        logger.info("Reading file: %r", EIA_filepath)
 
     FRS_iterator = read_in_chunks(FRS_filepath)
     # estimate size of chunk
     for df in FRS_iterator:
-        sz = len(df.to_string())
+        sz = int(len(df.to_string()) / 2)   # dividing by 4 to get approx good chunk number
         fullsz = os.stat(FRS_filepath)
         break
     print("Chunk Size (B), Total size (B): ", sz, fullsz.st_size)
@@ -110,7 +119,7 @@ def main(verbose=False, no_log=False, skip_rows=0, skip_chunks=0, outputfile=Non
     num_iterations = fullsz.st_size // sz
 
     FRS_iterator = read_in_chunks(FRS_filepath)
-    EIA_table = pd.read_csv(EIA_filepath)   # read completely to memory
+    EIA_table = pd.read_csv(EIA_filepath, encoding="utf-8")   # read completely to memory
     EIA_table = EIA_table.dropna()
 
     num_iter_to_skip = 0
@@ -125,36 +134,40 @@ def main(verbose=False, no_log=False, skip_rows=0, skip_chunks=0, outputfile=Non
     with enlighten.Counter(total=num_iterations, desc='Progress...', unit='ticks') as pbar:
         print("Starting comparisons...")
         for i, frs_chunk in enumerate(FRS_iterator):
+
+            if skip_rows > 0 and i < num_iter_to_skip:
+                pbar.update()
+                continue
+
             frs_chunk = frs_chunk.dropna()
-            if skip_rows > 0 and i < num_iter_to_skip: continue
             frs_items = combine_columns_as_str(frs_chunk, fields["FRS_FIELDS"])
+
+
+            frs_indices = frs_chunk[fields["INDICES_FIELD"]].astype(str).tolist()
             eia_items = combine_columns_as_str(EIA_table, fields["EIA_FIELDS"])
 
-            eia_items = eia_items[:10] # DEBUG FIXME
+            if start_after_index >= len(frs_items):
+                start_after_index = 0
+                continue    # dropna sometimes makes len < chunk_size
             if skip_rows > 0 and start_after_index > 0 and i == num_iter_to_skip:
-                frs_items = frs_items[frs_items.length > start_after_index]
-            # print(eia_items[:10])
+                frs_items = frs_items[start_after_index:]
+                frs_indices = frs_indices[start_after_index:]
+
             with enlighten.Counter(total=len(frs_items), desc='Processing Chunk {I}/{N}'.format(I=i, N=num_iterations), unit='ticks') as pbarmini:
-                for item in frs_items:
+                for index, item in zip(frs_indices, frs_items):
                     # p = process.extractOne(item, eia_items)
-                    logger.info(item)
-                    p = filtersortfuzz(eia_items, item, fields["FUZZ_THRESHOLD"])
+                    logger.info("%r", item)
+                    p = filtersortfuzz(eia_items, item, cutoff=fields["FUZZ_THRESHOLD"])
+
                     # print("Input: {INP}\nMatch: {MAT}\n\n".format(INP=item, MAT=p))
                     if p:
                         if verbose: print("Input: {INP}\nMatch: {MAT}\n\n".format(INP=item, MAT=p))
-                        if log: logger.info("Input: {INP}\nMatch: {MAT}\n\n".format(INP=item, MAT=p))
-
-                        write_row(p[0], outputfile)
+                        if log: logger.info("Input: %r\nMatch: %r\n\n", item, p)
+                        write_row(p[0], index, p[1], outputfile)
                     pbarmini.update()
 
             pbar.update()
 
-            # break
-
 
 if __name__ == "__main__":
-    # open file1 csv
-    FRS_filepath = "data/FRS_Facility.csv"
-    EIA_filepath = "data/EIAinventoryfacilitytable.csv"
-
-    # main(FRS_filepath, EIA_filepath)
+    main()
